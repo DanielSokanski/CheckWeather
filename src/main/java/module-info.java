191932391c096d7c83e02a1e9888c9ee4c4bcd93
Daniel Sokanski <daniel.sokanski.programista@gmail.com>
2021-11-22 22:16:37 +0100

    FXML

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,4 +1,9 @@
 module pl.com.danielsokanski {
     requires javafx.controls;
+    requires javafx.fxml;
     exports pl.com.danielsokanski;
+    opens main.java.pl.com.danielsokanski;
+    opens main.java.pl.com.danielsokanski.view;
+    opens main.java.pl.com.danielsokanski.controller;
+    opens main.java.pl.com.danielsokanski.model;
 }
---
+++ src/main/java/pl/com/danielsokanski/controller/MainWindowController.java
@@ -0,0 +1,13 @@
+package pl.com.danielsokanski.controller;
+
+
+public class MainWindowController {
+
+    public void searchMyCity() {
+        System.out.println("Szukam swojego miasta");
+    }
+
+    public void searchNewCity() {
+        System.out.println("Szukam nowego miasta");
+    }
+}
